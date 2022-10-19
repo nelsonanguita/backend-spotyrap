06de826d2d5a28ee9f48093e551e1eae150e2647
nelson anguita <nelsonanguita@gmail.com>
2022-10-19 12:17:03 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -18,6 +18,22 @@ const options = {
 }
 app.use(cors(options));
 
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+
+
+
+
+
+
+
 //routes
 app.get('/',(req, res)=>{
     res.send("funciona????")
